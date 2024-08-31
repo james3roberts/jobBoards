1f586d4bf570dce002c2951f62fbb61e1528a46b
James <james3roberts@gmail.com>
2024-08-31 08:49:55 -0600

    got it to work

--- app.py
+++ app.py
@@ -1,32 +1,30 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__) #start the flask app
 
-# Initialize language frequency dictionary
-language_frequency = {}
+# Dictionary to store the programming languages and their counts
+languages = {}
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        job_title = request.form['job_title']
+        languages_input = request.form['languages']
 
-@app.route('/add_job', methods=['POST'])
-def add_job():
-    job_name = request.form['job_name']
-    languages = request.form['languages'].split(',')
-
-    for language in languages:
-        language = language.strip().lower()  # Normalize language names
-        if language in language_frequency:
-            language_frequency[language] += 1
-        else:
-            language_frequency[language] = 1
-
-    return jsonify(language_frequency)
-
-@app.route('/study_plan')
-def study_plan():
-    ranked_languages = sorted(language_frequency.items(), key=lambda x: x[1], reverse=True)
-    return jsonify(ranked_languages)
+        # Split the languages by comma and strip any whitespace
+        languages_list = [lang.strip() for lang in languages_input.split(',')]
+        
+        # Update the language counts
+        for lang in languages_list:
+            if lang in languages:
+                languages[lang] += 1
+            else:
+                languages[lang] = 1
+    
+    # Sort the languages by count in descending order
+    sorted_languages = dict(sorted(languages.items(), key=lambda item: item[1], reverse=True))
+    
+    return render_template('index.html', languages=sorted_languages)
 
 if __name__ == '__main__':
     app.run(debug=True)
@@ -35,3 +33,5 @@ if __name__ == '__main__':
 #to start just type flask run
 #if that dont work try export FLASK_APP=app.py
 #or export FLASK_ENV=development    to use development mode.
+#this is running on http://127.0.000.1:5000   Just hold ctrl and lick the link
+#ctrl+c to stop running
